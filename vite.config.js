edac3ebd94da50fcc5108dbb3e765960ce591f0a
Connor Robetorye <crobetorye@gmail.com>
2021-10-30 23:48:41 -0700

    reorganize

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ dotenv.config();
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-const { PORT = 3001 } = process.env;
+const PORT = process.env.PORT || 5000;
 
 // https://vitejs.dev/config/
 export default defineConfig({
